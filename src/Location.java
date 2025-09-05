ac984a16262a74bf4dd24ca4a75fd44c535f9c70
gjf2a <ferrer@hendrix.edu>
2025-09-05 11:07:42 -0500

    Friday 9/5

---
+++ src/Location.java
@@ -0,0 +1,51 @@
+// North of equator is positive
+// East of prime meridian is positive
+
+public class Location {
+    private double latitude;
+    private double longitude;
+
+    public Location(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public boolean withinRectangle(Location southwest, Location northeast) {
+        if (latitude >= southwest.latitude && longitude >= southwest.longitude &&
+            latitude <= northeast.latitude && longitude <= northeast.longitude) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + latitude + ", " + longitude + ")";
+    }
+
+    public boolean equals(Object other) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void main(String[] args) {
+        Location conway = new Location(35.0918, -92.4367);
+        Location jonesboro = new Location(35.8357, -90.7051);
+        Location texarkana = new Location(33.4251, -94.0477);
+
+        System.out.println(conway.withinRectangle(texarkana, jonesboro));
+
+        System.out.println("Conway is at: " + conway);
+
+        Location hendrix = new Location(35.0918, -92.4367);
+        System.out.println(conway.equals(hendrix));
+    }
+}
---
+++ src/Scorekeeper.java
@@ -0,0 +1,35 @@
+public class Scorekeeper {
+    private int home;
+    private int away;
+
+    public Scorekeeper() {
+        home = 0;
+        away = 0;
+    }
+
+    public void score(boolean home) {
+        if (home) {
+            this.home += 1;
+        } else {
+            this.away += 1;
+        }
+    }
+
+    public boolean wins(boolean home) {
+        if (this.home == this.away) {
+            return false;
+        } else if (home) {
+            return this.home > this.away;
+        } else {
+            return this.away > this.home;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scorekeeper score = new Scorekeeper();
+        score.score(true);
+        score.score(true);
+        score.score(false);
+        System.out.println("Did Home win? " + score.wins(true));
+    }
+}
